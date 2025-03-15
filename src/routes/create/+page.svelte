2c2e934c869c3f76a0c8e14021978abd278bb05c
Adiro777 <112520123+Adiro777@users.noreply.github.com>
2025-03-15 14:58:25 -0700

    Added onto create

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,3 +1,53 @@
-<a href="/">go back</a>
+<script>
+    let muralTheme = ""
+    let dims = ""
 
-<p>create page</p>
+    import { goto } from '$app/navigation';
+    function GoBack() {goto('/')}
+</script>
+
+<button class="btn btn-primary" onclick={GoBack}>Go Back</button>
+
+<div class="general">
+    <div class="theme">
+        <h1 class="text-5xl font-bold">Mural Theme</h1>
+        <input bind:value={muralTheme} type="text" placeholder="Theme" class="input input-neutral">
+    </div>
+    
+    <div>
+        <h1 class="text-5xl font-bold">Dimensions</h1>
+        <select bind:value={dims} class="select select-neutral">
+            <option>2 x 2</option>
+            <option>3 x 3</option>
+            <option>4 x 4</option>
+        </select>
+    </div>
+
+    <button class="btn btn-neutral">Create Mural</button>
+    
+</div>
+
+<style>
+    h1 {
+        margin-bottom: 20px;
+    }
+
+    .general {
+        position: absolute;
+        top: 30%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        flex-direction: column; 
+        align-items: center;
+        gap: 20px; 
+        width: 400px; /* Set a larger width */
+    }
+
+    .theme {
+        margin-bottom: 50px;
+    }
+
+    
+</style>
+
