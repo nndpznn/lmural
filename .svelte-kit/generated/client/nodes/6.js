fffff1f6a98ebd206718bd44a09bc4d0f658bf49
Eren Unsal <aaronunsal@gmail.com>
2025-03-15 15:46:01 -0700

    Untracking annoying things

--- .svelte-kit/generated/client/nodes/6.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< Updated upstream
-export { default as component } from "../../../../src/routes/+page.svelte";
-=======
-export { default as component } from "../../../../src/routes/active/+page.svelte";
->>>>>>> Stashed changes
