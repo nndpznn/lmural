b0030190f9600477e359b223f96cfceb100c015e
Eren Unsal <aaronunsal@gmail.com>
2025-03-15 14:56:51 -0700

    Removed duplicate create function

--- src/types/tile.ts
+++ src/types/tile.ts
@@ -1,47 +1,43 @@
 import { supabase } from "$lib/supabaseClient";
 
 class Tile {
-  id: number;
-  artistName: string;
-  artistRegion: string;
-  position: number;
-  muralID: number;
+	id: number
+	artistName: string
+	artistRegion: string
+	position: number
+	muralId: number
 
-  // content: // Custom Data Type to be added later
+	// content: // Custom Data Type to be added later
+  
+	constructor(artistName: string, artistRegion: string, position: number, muralId: number) {
+	  this.id = 0
+	  this.artistName = artistName
+	  this.artistRegion = artistRegion
+	  this.position = position
+	  this.muralId = muralId
 
-  constructor(
-    artistName: string,
-    artistRegion: string,
-    position: number,
-    muralID: number
-  ) {
-    this.id = 0;
-    this.artistName = artistName;
-    this.artistRegion = artistRegion;
-    this.position = position;
-    this.muralID = muralID;
+	}
+
+		async create() {
+			const { data, error } = await supabase
+			  .from("tiles")
+			  .insert([
+				{
+				  artistName: this.artistName,
+				  artistRegion: this.artistRegion,
+				  position: this.position,
+				  muralId: this.muralId
+				}
+			  ])
+			  .select("id")
+	
+			if (error) {
+				console.error("Oopsie occurred while entering tiles:", error)
+				return;
+			}
+	
+			this.id = data[0].id
+		}
   }
-
-  async create() {
-    const { data, error } = await supabase
-      .from("tiles")
-      .insert([
-        {
-          artistName: this.artistName,
-          artistRegion: this.artistRegion,
-          position: this.position,
-          muralID: this.muralID,
-        },
-      ])
-      .select("id");
-
-    if (error) {
-      console.error("Oopsie occurred while entering tiles:", error);
-      return;
-    }
-
-    this.id = data[0].id;
-  }
-}
 
 export default Tile;
